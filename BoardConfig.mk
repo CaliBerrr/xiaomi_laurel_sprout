66f26ded7ad66c91c2a749b124f8dad3b9dfbb6b
CaliBerrr <mominulislamgit01@gmail.com>
2020-12-25 11:57:14 +0000

    BoardConfig:Include Logcat(Debug Purposes)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,3 +111,7 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# Log Flags(Debug Purposes)
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
